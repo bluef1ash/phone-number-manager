21da85c71e19cbed4ddc1953d33543f2c1f078a2
bluef1ash <liangtian_2005@163.com>
2022-07-23 17:23:10 +0800

    升级 Spring Security 新版配置

--- backend/src/main/java/com/github/phonenumbermanager/config/WebSecurityConfig.java
+++ backend/src/main/java/com/github/phonenumbermanager/config/WebSecurityConfig.java
@@ -2,16 +2,15 @@ package com.github.phonenumbermanager.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -21,7 +20,6 @@ import com.github.phonenumbermanager.constant.SystemConstant;
 import com.github.phonenumbermanager.security.JwtAuthenticationEntryPoint;
 import com.github.phonenumbermanager.security.filter.JwtTokenFilter;
 import com.github.phonenumbermanager.security.handler.JwtAccessDeniedHandler;
-import com.github.phonenumbermanager.service.SystemUserService;
 
 import lombok.AllArgsConstructor;
 
@@ -32,47 +30,40 @@ import lombok.AllArgsConstructor;
  */
 @AllArgsConstructor
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    private final SystemUserService systemUserService;
+public class WebSecurityConfig {
     private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
     private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
     private final JwtTokenFilter jwtTokenFilter;
 
     @SuppressWarnings("all")
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().addFilterBefore(corsFilter(), UsernamePasswordAuthenticationFilter.class)
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.csrf().disable().addFilterBefore(corsFilter(), UsernamePasswordAuthenticationFilter.class)
             .exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint)
             .accessDeniedHandler(jwtAccessDeniedHandler).and().headers().frameOptions().sameOrigin().and()
             .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-            .addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class).authorizeRequests()
-            .antMatchers(SystemConstant.PERMIT_WHITELIST).permitAll().antMatchers(SystemConstant.ANONYMOUS_WHITELIST)
-            .anonymous().anyRequest().access("@systemUserService.hasPermission(request, authentication)");
+            .addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class)
+            .authorizeRequests(authorizeRequest -> authorizeRequest.antMatchers(SystemConstant.PERMIT_WHITELIST)
+                .permitAll().antMatchers(SystemConstant.ANONYMOUS_WHITELIST).anonymous().anyRequest()
+                .access("@systemUserService.hasPermission(request, authentication)"))
+            .build();
     }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(authProvider());
-        super.configure(auth);
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+        throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 
-    @Override
-    public void configure(WebSecurity web) {
-        web.ignoring().antMatchers(HttpMethod.OPTIONS, "/**").antMatchers(SystemConstant.PERMIT_WHITELIST);
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (webSecurity) -> webSecurity.ignoring().antMatchers(HttpMethod.OPTIONS, "/**")
+            .antMatchers(SystemConstant.PERMIT_WHITELIST);
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public AuthenticationProvider authProvider() {
-        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
-        daoAuthenticationProvider.setHideUserNotFoundExceptions(false);
-        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
-        daoAuthenticationProvider.setUserDetailsService(systemUserService);
-        return daoAuthenticationProvider;
     }
 
     @Bean
